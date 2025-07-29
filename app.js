752c06ab086d074af6f3f8aae59e6565b39121a8
Prince <princesinha629@gmail.com>
2025-07-29 22:50:02 +0530

    centralized error handling middleware

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ import authRouter from './routes/auth.route.js';
 import userRouter from './routes/user.route.js';
 import subscriptionRouter from './routes/subscription.route.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// express inbuilt middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
-
+// express routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+
+// centralized error handling middleware
+app.use(errorMiddleware)
 
 app.get("/",(req,res) => {
     res.status(200).send("Welcome to Subscription Tracking API");
