c8873af4fdca4502b5332dcf9445b321a0448346
Prince <princesinha629@gmail.com>
2025-07-30 00:28:25 +0530

    authorization

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 
-userRouter.get("/",(req,res) => res.send({title: "Get all users"}));
-userRouter.get("/:id",(req,res) => res.send({title: "Get user details"}));
+userRouter.get("/", getUsers);
+userRouter.get("/:id", authorize, getUser);
 userRouter.post("/",(req,res) => res.send({title: "Create a new user"}));
 userRouter.put("/:id",(req,res) => res.send({title: "Update user"}));
 userRouter.delete("/:id",(req,res) => res.send({title: "Delete user"}));
